876d7982c7d88e08678810857b90d6bba364b050
DaniloAC <daniloac95@gmail.com>
2018-10-15 17:03:52 -0500

    Operaciones con arreglos
    aprendimos a sacar el indice de un arreglo, imprimir arreglos, destructurarles y unirles sin utilizacion de for ni while

---
+++ 01-javascript/02-arreglos.js
@@ -0,0 +1,53 @@
+var arregloNumeros = [1, 2, 3, 4];
+
+console.log(arregloNumeros[2]);
+
+console.log(arregloNumeros);
+
+arregloNumeros.splice(1,0,4);
+
+console.log(arregloNumeros);
+
+arregloNumeros.splice(1,0,4,5,6,7,8,9,10);
+
+console.log(arregloNumeros);
+
+var indiceNumeroSeis = arregloNumeros.indexOf(6);
+
+arregloNumeros.splice(indiceNumeroSeis,1);
+
+console.log(arregloNumeros);
+
+var arregloUno = arregloNumeros.slice(0, 3);
+var arregloDos = arregloNumeros.slice(3, 6);
+
+var arregloUnoDos = [1, 2];
+var arregloSeis = [6];
+
+//Destructuracion de arreglos
+
+console.log(...arregloUnoDos);
+
+
+var arregloTotal = [...arregloUnoDos,
+    ...arregloUno,
+    ...arregloSeis,
+    ...arregloDos];
+console.log(arregloTotal);
+
+var arregloSiguientesNumeros = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+arregloTotal.splice(arregloTotal.length,0, ...arregloSiguientesNumeros);
+console.log(arregloTotal);
+
+
+
+//[0,10] cerrado esta  el 10
+
+//arregloDos.push(7);
+//var indiceSiete = arregloDos.indexOf(7);
+
+//console.log(arregloUno);
+//console.log(arregloDos);
+//console.log(indiceSiete);
+
+
